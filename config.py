9d664a2625946f42ec2c557d7d90fa53461219a3
nero-bc <126649764+nero-bc@users.noreply.github.com>
2024-07-07 18:39:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
+
 import os
 
 class Config(object):
     API_HASH = os.environ.get("API_HASH")
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    TELEGRAM_API = os.environ["TELEGRAM_API"]
+    API_ID = os.environ.get("API_ID")
     OWNER = os.environ.get("OWNER")
     OWNER_USERNAME = os.environ.get("OWNER_USERNAME")
     PASSWORD = os.environ.get("PASSWORD")
@@ -11,9 +12,13 @@ class Config(object):
     LOGCHANNEL = os.environ.get("LOGCHANNEL")  # Add channel id as -100 + Actual ID
     GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID", "root")
     USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", None)
+    
     IS_PREMIUM = True
-    MODES = ["video-video", "video-audio", "video-subtitle", "extract-streams"]
+    PAID_BOT = os.environ.get("PAID_BOT")
     
     BOT_USERNAME = os.environ.get("BOT_USERNAME")
     SHORTENER_SITE = os.environ.get("SHORTENER_SITE")
     SHORTENER_API = os.environ.get("SHORTENER_API")
+    PAID_PROMOTION = os.environ.get("PAID_PROMOTION")
+
+    MODES = ["video-video", "video-audio", "video-subtitle", "extract-streams"]
